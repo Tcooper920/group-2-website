ffac52e382eb57f70fc6877fe17f484a05920c5b
Thomas Cooper <thomascooper@Thomass-MacBook-Pro.local>
2020-02-27 07:27:55 -0500

    Tweaked the nav bar to automatically close when window size is scaled down to the mobile width

--- group-2.js
+++ group-2.js
@@ -26,12 +26,18 @@ function displayHiddenNavLinksOnDesktop() {
 
 	var numberOfLinks = document.getElementsByClassName("page-links");
 
-	if (window.innerWidth > 450) {
+	if (window.innerWidth >= 450) { // display links on desktop view
 		for (var i = 0; i < numberOfLinks.length; i++) {
 			document.getElementsByTagName("nav")[0].getElementsByClassName("page-links")[i].style.display = "block";
+		}
+	} else {
+		for (var i = 0; i < numberOfLinks.length; i++) { // hide links on mobile view
+			document.getElementsByTagName("nav")[0].getElementsByClassName("page-links")[i].style.display = "none";
 		}
 	}
 
 }
 
 window.onresize = displayHiddenNavLinksOnDesktop;
+
+
