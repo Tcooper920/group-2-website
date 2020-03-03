4fa08e04d74153bedc32d576435ff2b7983ad435
Emily York <emily@Emilys-MacBook-Pro.local>
2020-03-02 19:39:48 -0500

    Javascript functionality for donation amount buttons

--- group-2.js
+++ group-2.js
@@ -4,3 +4,21 @@ var naming = document.getElementById('naming');
 sign.addEventListener("click", function() {
 	alert(naming.value + " thank you for signing the pledge");
 })
+
+
+var five = document.getElementById('five');
+five.addEventListener("click", function() {
+	alert("Thank you for committing to donate " + five.value);
+})
+
+var ten = document.getElementById('ten');
+ten.addEventListener("click", function() {
+	alert("Thank you for committing to donate " + ten.value);
+})
+
+var twenty = document.getElementById('twenty');
+twenty.addEventListener("click", function() {
+	alert("Thank you for committing to donate " + twenty.value);
+})
+
+
