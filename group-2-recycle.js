fb9d360e9a2020ffd878500a1696b3c443d67363
Rosemary Meyers <rmeyers2019@Rosemarys-MacBook-Pro-2.local>
2020-03-11 23:28:44 -0400

    playing withjavascript and added html form fields

--- group-2-recycle.js
+++ group-2-recycle.js
@@ -6,40 +6,37 @@
 
 var spinner = document.getElementsByClassName("threeRsWhite");
 
-var R1 = document.getElementsByClassName("R1");
+var R1 = document.getElementById("R1");
 
-var R2 = document.getElementsByClassName("R2");
+var R2 = document.getElementById("R2");
 
-var R3 = document.getElementsByClassName("R3");
+var R3 = document.getElementById("R3");
 
-var R1_Explained = document.getElementsByClassName("R1_Explained");
+var R1_Explained = document.getElementById("R1_Explained");
 
-var R2_Explained = document.getElementsByClassName("R2_Explained");
+var R2_Explained = document.getElementById("R2_Explained");
 
-var R3_Explained = document.getElementsByClassName("R3_Explained");
+var R3_Explained = document.getElementById("R3_Explained");
 
 
 var RSymbol;
 
-R1.addEventListener("click", function(){
-    RSymbol = "pick1"
+
+R1.addEventListener(“click”, function () {
+    console.log("hi");
 });
 
-R2.addEventListener("click", function(){
-    RSymbol = "pick2"
+
+
+/*R1.addEventListener(“click”, function(){
+document.getElementById(“R1”).style.display = “block”;
+document.getElementById(“R2”).style.opacity = “.5”;
+document.getElementById(“R3”).style.opacity = “.5”;
+document.getElementById(“R1_Explained”).style.display = “block”;
+document.getElementById(“R2_Explained”).style.display = “none”;
+document.getElementById(“R3_Explained”).style.display = “none”;
 });
-
-R3.addEventListener("click", function(){
-    RSymbol = "pick3"
-});
-
-function recyclePicks(R1, R2, R3){
-    if(RSymbol == "pick1"){
-        R2.style.color = "000"
-    }
-}
-R2.style.color = "000"
-
+*/
 
 
 
