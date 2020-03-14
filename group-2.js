3c33c23be267373e9a9d490f17ce4d5ac1a4d112
Thomas Cooper <thomascooper@Thomass-MBP.fios-router.home>
2020-03-14 17:43:47 -0400

    Updated my homepage with Emily's lightbox

--- group-2.js
+++ group-2.js
@@ -52,7 +52,7 @@ window.onscroll = function() {
 		for (var i = 0; i < numberOfLinks.length; i++) { // hide links in nav bar
 			document.getElementsByTagName("nav")[0].getElementsByClassName("page-links")[i].style.display = "none";
 		}
-		document.getElementsByTagName("nav")[0].style.top = "-3rem"; // hide nav bar
+		document.getElementsByTagName("nav")[0].style.top = "-6rem"; // hide nav bar
 	} else {
 		
 		if (window.innerWidth >= 650) { // hide links by default if window resized from desktop to mobile width
@@ -71,3 +71,26 @@ var backToTopBtn = document.getElementById("back-top-top");
 backToTopBtn.addEventListener("click", function() {
 	window.scrollTo(0, 0);
 });
+
+/* Donation light box *********/
+document.getElementById("donateButton").addEventListener("click", function() {
+  document.getElementById("lightbox").className = "open";
+});
+
+/* if footer donation button exists, add event listener */
+if (document.getElementsByClassName("donation-button")[0] !== undefined) {
+	document.getElementsByClassName("donation-button")[0].addEventListener("click", function() { /* Footer donation button */
+  		document.getElementById("lightbox").className = "open";
+	});
+}
+
+document.getElementById("close").addEventListener("click", function() {
+  document.getElementById("lightbox").className = "";
+});
+
+document.getElementById("lightbox").addEventListener("click", function(e) {
+  if (e.target.id == "lightbox") {
+    document.getElementById("lightbox").className = "";
+  }
+});
+
